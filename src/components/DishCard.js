02e997b014b672f15af0610326db2d3881481d8a
Ashwin Krishnan <9294975+ashwinknan@users.noreply.github.com>
2023-08-21 11:29:52 +0530

    Minor Style Changes to DishCard

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const dishDisplayNames = {
-    breakfast: "Breakfast",
-    salad: "Salad",
-    lunchDinner: "Lunch + Dinner"
-};
-
-const DishCard = ({ type, dish }) => (
-    <div className="col-md-4 mb-4">
-        <div className="card h-100 shadow">
-            <div className="card-body text-center">
-                <h3 className="card-title">{dishDisplayNames[type]}</h3>
-                <p className="card-text">{dish.Name}</p>
-                <ul className="list-group list-group-flush">
+function DishCard({ type, dish }) {
+    return (
+        <div className="col-md-4">
+            <div className="card mb-4">
+                {/* Title */}
+                <div className="card-header bg-primary text-white dish-card-title">
+                    {capitalizeFirstLetter(type)}
+                </div>
+                {/* Dish Name */}
+                <h5 className="card-title dish-card-dish-name mt-3">{dish.Name}</h5>
+                {/* Ingredients */}
+                <ul className="list-group list-group-flush dish-card-ingredients">
                     {dish.Ingredients.map((ingredient, index) => (
                         <li key={index} className="list-group-item">{ingredient}</li>
                     ))}
                 </ul>
             </div>
         </div>
-    </div>
-);
+    );
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).replace(/Dinner$/, ' + Dinner');
+}
 
 export default DishCard;
+
