6fdb3bebb86276e68c1bcd78c91306314c979772
Ashwin Krishnan <9294975+ashwinknan@users.noreply.github.com>
2023-08-21 20:23:59 +0530

    Added a refresh button by updating card header div and styling on hover

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as RefreshIcon } from 'bootstrap-icons/icons/arrow-repeat.svg';
+
 
 function DishCard({ type, dish, ingredients }) {
     const ingredientString = ingredients[dish.Name];
@@ -7,8 +9,11 @@ function DishCard({ type, dish, ingredients }) {
     return (
         <div className="col-md-4">
             <div className="card mb-4">
-                <div className="card-header bg-primary text-white">
-                    {type.charAt(0).toUpperCase() + type.slice(1)}
+                <div className="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+                 {type.charAt(0).toUpperCase() + type.slice(1)}
+                    <button className="btn-refresh" title="Generate another dish">
+                        <RefreshIcon className="bi bi-arrow-repeat" />
+                    </button>
                 </div>
                 <div className="card-body">
                     <h5 className="card-title">{dish.Name}</h5>
